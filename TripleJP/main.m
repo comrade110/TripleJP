005622734b31ef6d5a0afbb94cda41f0697292a9
Lowson Chen <comrade213@sina.com>
2012-07-25 11:28:01 +0800

    Initial Commit

---
+++ TripleJP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TripleJP
+//
+//  Created by user on 12-7-25.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}

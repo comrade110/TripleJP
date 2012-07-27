b413b6d6997f08eb667c7b26f0e29d18f0315402
Lowson Chen <comrade213@sina.com>
2012-07-27 10:05:41 +0800

    temp

---
+++ ObjClass.h
@@ -0,0 +1,20 @@
+//
+//  ObjClass.h
+//  TripleJP
+//
+//  Created by user on 12-7-26.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface ObjClass : CCNode {
+    
+    
+    
+}
+
+-(id)createObj:(NSString *)objNum;
+
+@end
---
+++ ObjClass.m
@@ -0,0 +1,42 @@
+//
+//  ObjClass.m
+//  TripleJP
+//
+//  Created by user on 12-7-26.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ObjClass.h"
+
+
+@implementation ObjClass
+
+-(id)init{
+
+    self = [super init];
+    
+    if (self != nil) {
+        
+        
+    }
+    
+    return self;
+}
+
+-(id)createObj:(NSString *)objNum{
+
+    cao     660
+    guanmu  160
+    tree    25
+    wuzi    10
+    mao     100
+    feimao  10
+    zhadan  20
+    bian    15          reflashUnit ID 
+    arc4random()%1000-> getPlist-String -> getTouchUpCoordinate(x,y) -> isEmpty ->
+
+    (x-1,y-1),(x,y-1),(x,y-2),(x+1,y-1),(x+1,y),(x+2,y),(x+1,y+1),(x,y+1),(x,y+2),(x-1,y+1),(x-1,y),(x-2,y) 
+    
+}
+
+@end
